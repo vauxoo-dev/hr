87961b2ba57752ff8aac7df7da63d7bcdfb4ea19
moylop260 <moylop260@vauxoo.com>
2014-10-21 13:50:10 -0500

    [ADD][hr_loan] Add first commit of module hr_loan

---
+++ hr_loan/model/hr_loan.py
@@ -0,0 +1,178 @@
+#!/usr/bin/python
+# -*- encoding: utf-8 -*-
+###############################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#    Copyright (C) OpenERP Venezuela (<http://www.vauxoo.com>).
+#    All Rights Reserved
+############# Credits #########################################################
+#    Coded by: Yanina Aular <yani@vauxoo.com>
+#    Planified by: Moises Lopez <moises@vauxoo.com>
+#    Audited by: Humberto Arocha <hbto@vauxoo.com>
+###############################################################################
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+###############################################################################
+
+from openerp import pooler, tools
+from openerp.osv import fields, osv
+from openerp.tools.translate import _
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+from datetime import timedelta
+
+PAYMENT_TYPE = [
+    ('fortnightly', 'Fortnightly'),
+    ('monthly', 'Monthly'),
+    ('bimonthly', 'Bi-Monthly'),
+    ('weekly', 'Weekly'),
+]
+
+STATE_LOAN = [
+    ('draft', 'Draft'),
+    ('active', 'Active'),
+    ('cancel', 'Cancel'),
+    ('done', 'Done'),
+]
+
+
+class hr_loan(osv.Model):
+    _name = 'hr.loan'
+    _columns = {
+        'name': fields.char('Name', translate=True),
+        'amount_approved': fields.float('Amount approved', help='Amount approved \
+            for loan'),
+        'payment_type': fields.selection(PAYMENT_TYPE, 'Payment Type'),
+        'partner_id': fields.many2one('res.partner', 'Bank'),
+        'employee_id': fields.many2one('hr.contract', 'Employee Contract'),
+        'date_start': fields.date('Start Date'),
+        'date_stop': fields.date('End Date'),
+        'currency_id': fields.many2one('res.currency', 'Currency'),
+        'state': fields.selection(STATE_LOAN, 'State'),
+        'share_ids': fields.one2many('hr.loan.line', 'hr_loan_id', 'Shares'),
+        'share_quantity': fields.integer('Share Quantity'),
+    }
+
+    _defaults = {
+            'state' : 'draft',
+            'currency_id': lambda s, c, u, ctx:
+                s.pool.get('res.users').browse(c, u, u, context=ctx).company_id.currency_id.id,
+            }
+    def activate_loan(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        ids = isinstance(ids,(int,long)) and [ids] or ids
+        for brw in self.browse(cr, uid, ids,  context=context):
+            self.compute_shares(cr, uid, [brw.id], context=context)
+            self.write(cr, uid, [brw.id], {'state':'active'}, context=context)
+        return True
+
+    def draft_loan(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        ids = isinstance(ids,(int,long)) and [ids] or ids
+        for brw in self.browse(cr, uid, ids,  context=context):
+            self.write(cr, uid, [brw.id], {'state':'draft'}, context=context)
+        return True
+
+    def cancel_loan(self, cr, uid, ids, context=None):
+        if context is None:
+            context = {}
+        ids = isinstance(ids,(int,long)) and [ids] or ids
+        for brw in self.browse(cr, uid, ids,  context=context):
+            self.write(cr, uid, [brw.id], {'state':'cancel'}, context=context)
+        return True
+
+    def last_day_of_month(self, date):
+        if date.month == 12:
+            return date.replace(day=31)
+        return date.replace(month=date.month+1, day=1) - timedelta(days=1)
+
+    def compute_shares(self, cr, uid, ids, context=None):
+        hr_loan_line_obj = self.pool.get('hr.loan.line')
+        for hr_loan in self.browse(cr, uid, ids, context=context):
+            old_loanline_ids = hr_loan_line_obj.search(cr, uid, [('hr_loan_id', '=', hr_loan.id)], context=context)
+            if old_loanline_ids:
+                hr_loan_line_obj.unlink(cr, uid, old_loanline_ids, context=context)
+
+            if hr_loan.amount_approved <= 0 or hr_loan.share_quantity <= 0:
+                raise osv.except_osv( _("Values not allowed"),
+                    _('Amount Approved and Share Quantity must be greater than zero'))
+
+            share = hr_loan.amount_approved / hr_loan.share_quantity
+
+            ds = datetime.strptime(hr_loan.date_start, '%Y-%m-%d')
+            for i in xrange(0, hr_loan.share_quantity):
+
+                if hr_loan.payment_type == 'fortnightly':
+                    if ds.day < 15:
+                        ds = ds.replace(day=15)
+                    elif ds.day == 15:
+                        ds = self.last_day_of_month(ds)
+                    elif ds == self.last_day_of_month(ds):
+                        ds = ds + relativedelta(days=15)
+                if hr_loan.payment_type == 'weekly':
+                    ds = ds + relativedelta(days=7)
+                if hr_loan.payment_type == 'monthly':
+                    ds = ds + relativedelta(days=1)
+                    ds = self.last_day_of_month(ds)
+                if hr_loan.payment_type == 'bimonthly':
+                    if i == 0:
+                        ds = ds + relativedelta(days=1)
+                        ds = self.last_day_of_month(ds)
+                    else:
+                        ds = ds + relativedelta(months=2)
+                        ds = self.last_day_of_month(ds)
+
+                hr_loan_line_obj.create(cr, uid, {
+                        'name':  "%s %s" % (_('Share'), ds.strftime('%Y-%m-%d')),
+                        'payment_date': ds,
+                        'state': 'unpaid',
+                        'hr_loan_id': hr_loan.id,
+                        'share': share,
+                    }, context=context)
+
+            self.write(cr, uid, [hr_loan.id], {'date_stop': ds + relativedelta(days=1)}, context=context)
+        return True
+
+class hr_loan_line(osv.Model):
+    _name = 'hr.loan.line'
+    _columns = {
+        'name': fields.char('Name'),
+        'hr_loan_id': fields.many2one('hr.loan', 'Loan ID'),
+        'hr_payslip_id': fields.many2one('hr.payslip', 'Payslip ID'),
+        'payment_date': fields.date('Payment Date'),
+        'share': fields.float('Share', help='Share to pay'),
+        'payslip_line_id': fields.many2one('hr.payslip.line', 'Payslip Line'),
+        'partner_id': fields.related('payslip_line_id', 'partner_id',
+                                     type='many2one', string='Bank',
+                                     relation="res.partner"),
+        #~'paid': fields.boolean('Paid'),
+        'state': fields.selection([('unpaid', 'Unpaid'), ('paid', 'Paid')], 'State'),
+
+    }
+
+class hr_payslip(osv.Model):
+
+    _inherit = 'hr.payslip'
+
+    _columns = {
+
+        'share_line_ids': fields.one2many('hr.loan.line', 'hr_payslip_id',
+            'Share Line'),
+
+        #~'total_loan': fields.function(_total_loan,
+        #~    digits_compute=dp.get_precision('Account'), string='Total Loan',
+        #~    store={
+        #~        'hr.payslip': (lambda self, cr, uid, ids, c={}: ids, ['share_line_ids'], 20),
+        #~    },
+    }
