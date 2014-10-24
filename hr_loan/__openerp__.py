b08cbeecd0518bdcd21805d94347f94ab5a0a04d
Yanina Aular (Vauxoo) <yanina.aular@vauxoo.com>
2014-10-24 16:50:41 -0430

    [FIX] load view before security

--- hr_loan/__openerp__.py
+++ hr_loan/__openerp__.py
@@ -37,10 +37,10 @@ This module will manage the loans to employees of the company in OpenERP.
 ''',
     'depends': ['hr', 'hr_payroll'],
     'data': [
-        'security/hr_loan_security.xml',
-        'security/ir.model.access.csv',
         'view/hr_loan_view.xml',
         'data/hr_loan.xml',
+        'security/hr_loan_security.xml',
+        'security/ir.model.access.csv',
     ],
     'demo': [],
     'test': [],
