020181b8e4cb11753f50673c9c5d978d0081c7a0
luistorresm <luis_t@vauxoo.com>
2014-10-28 16:16:15 -0600

    [REF][hr_passport_employee]Order __openerp__.py and add file es_PA

--- hr_passport_employee/__openerp__.py
+++ hr_passport_employee/__openerp__.py
@@ -23,22 +23,29 @@
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 #
-{"name": "HR passport employee",
+{
+    "name": "HR passport employee",
     "version": "1.0",
-    "depends": ["hr",
-                ],
     "author": "Vauxoo",
+    "category": "HR",
     "description": """
 This module add expedition date and expiration of passport
-    """,
+
+""",
     "website": "http://www.vauxoo.com",
-    "category": "HR",
+    "license": "",
+    "depends": [
+        "hr"
+    ],
     "demo": [],
     "data": [
         "view/hr_employee_view.xml"
     ],
     "test": [],
-    "images": [],
-    "auto_install": False,
-    "application": True,
-    "installable": True, }
+    "js": [],
+    "css": [],
+    "qweb": [],
+    "installable": True,
+    "auto_install": False
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
