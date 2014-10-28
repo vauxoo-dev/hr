4711ccf37f174e7a874657dd839491029a63043e
Katherine Zaoral <kathy@vauxoo.com>
2014-10-28 15:59:33 -0430

    [ADD] add data ir.cron record.

--- hr_working_hours_switch/__openerp__.py
+++ hr_working_hours_switch/__openerp__.py
@@ -40,6 +40,7 @@ HR Working Hours Switch
         'hr_attendance',
     ],
     'data': [
+        'data/hr_working_hours_switch.xml',
         'security/ir.model.access.csv',
         'view/hr_working_hours_switch_view.xml',
         'view/hr_contract_view.xml',
